30cb19ca97b81320dbf360ba1cfe5efe0cc40385
jeff-tor <jjiaaffuu@hotmail.com>
2022-09-03 17:06:56 +0800

    project files

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return jsonify({"Message": "How you doin'"})
+    return jsonify({"Message": "WELCOME TO DOCKER TUTORIAL WELCOME TO KUBERNETES'"})
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
